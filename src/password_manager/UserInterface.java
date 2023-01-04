e7b5cd51ce1e9ddd36184e97365fec2fb3f8a571
Naïm Giger <naim.giger@outlook.com>
2023-01-04 20:39:21 +0100

    create UserInterface utility

--- src/password_manager/UserInterface.java
+++ src/password_manager/UserInterface.java
@@ -1,7 +1,19 @@
 package password_manager;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    public static final String PROMPT_SYMBOL = "> ";
+
     public void run() {
 
+    }
+
+    private String prompt(String message) {
+        System.out.println(message);
+        System.out.print(PROMPT_SYMBOL);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
